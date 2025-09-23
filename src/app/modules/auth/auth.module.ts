497f8c2c053d3ae3f42c43275a394eb3db3b1164
Pablo Vidal <pabloalejandrovidal@gmail.com>
2025-09-23 19:47:40 -0300

    refactor(auth): integrate Angular Material in auth module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -6,6 +6,10 @@ import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -20,7 +24,11 @@ import { AuthRoutingModule } from './auth-routing.module';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterOutlet
+    RouterOutlet,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class AuthModule { }
