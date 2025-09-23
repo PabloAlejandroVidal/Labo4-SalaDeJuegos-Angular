37dbadcf0c911f016c40b4728f68f2a3171e794a
Pablo Vidal <pabloalejandrovidal@gmail.com>
2025-09-23 19:52:33 -0300

    refactor(encuestas): apply Angular Material styles to encuesta pages

--- src/app/pages/encuestas-respuestas/encuestas-respuestas.component.ts
+++ src/app/pages/encuestas-respuestas/encuestas-respuestas.component.ts
@@ -2,11 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Encuesta } from 'app/shared/interfaces/encuesta';
 import { EncuestaService } from 'app/shared/services/encuesta/encuesta.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @Component({
   selector: 'app-encuestas-respuestas',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatListModule, MatDividerModule, MatProgressSpinnerModule],
   templateUrl: './encuestas-respuestas.component.html',
   styleUrl: './encuestas-respuestas.component.scss'
 })
